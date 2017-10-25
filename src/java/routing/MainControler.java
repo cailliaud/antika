48ad9ff7c4ca4034efabd3d8cc6ba55d54bf005a
Cailliaud <cailliaud.a@gmail.com>
2017-10-25 08:54:25 +0200

    début du routing

---
+++ src/java/routing/MainControler.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package routing;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author caill
+ */
+@Named(value = "mainControler")
+@Dependent
+public class MainControler {
+
+    /**
+     * Creates a new instance of MainControler
+     */
+    public MainControler() {
+    }
+    
+    public String goToCreatePerso(){
+        return "createPers";
+    }
+    
+}
